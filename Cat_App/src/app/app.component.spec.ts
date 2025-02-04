4fe154d82707e18b1b7be4eee71032d2c1c15032
eimanncalderon0711 <calderon.eimann0711@gmail.com>
2025-02-04 21:01:27 +0800

    updated the spec file for app component test

--- Cat_App/src/app/app.component.spec.ts
+++ Cat_App/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent ],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
@@ -24,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Cat_App');
+    expect(compiled.querySelector('.navbar-brand')?.textContent).toContain('Cat_App');
   });
 });
